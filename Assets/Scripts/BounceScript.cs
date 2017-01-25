21c5b452df384dbed64e0e91dec7570deb8b75b3
zippyl <zippy@ex.ua>
2017-01-25 23:56:54 +0200

    hz

--- Assets/Scripts/BounceScript.cs
+++ Assets/Scripts/BounceScript.cs
@@ -9,30 +9,29 @@ public class BounceScript : MonoBehaviour {
     bool hasSpawned = false;
 
     // Use this for initialization
-    void Start()
-    {
+    void Start() {
         spawner = GameObject.FindGameObjectWithTag("Respawn");
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
 
     }
 
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.GetComponent<Rigidbody2D>().velocity.y < 0)
-        {
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.GetComponent<Rigidbody2D>().velocity.y < 0) {
             other.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
             other.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounceFactor, ForceMode2D.Impulse);
 
-            if (!hasSpawned)
-            {
+            if (!hasSpawned) {
                 //spawner.SendMessage("Spawn");
                 hasSpawned = true;
             }
         }
-        //GetComponent<AudioSource>().Play();
+
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        GetComponent<AudioSource>().Play();
     }
 }
